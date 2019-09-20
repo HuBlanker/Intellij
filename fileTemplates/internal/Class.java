b457981a362a831890c8306e2761b555e6bebf79
liupanfeng <liupanfeng@kanzhun.com>
2019-09-20 14:04:13 +0800

    IU-2018.3.5 <pfliu@bogon Create baseRefactoring.xml, vim_settings.xml, ide.general.xml, project.default.xml, equalsHashCodeTemplates.xml, lombok-plugin.xml, other.xml, Default.xml, find.xml, ui.lnf.xml, filetypes.xml, projectView.xml, AndroidComments.xml, plugin_ui.xml, colors.scheme.xml, KeyPromoterXSettings.xml, path.macros.xml, databaseDrivers.xml, templates.xml, GrepConsole.xml, huyanshi.xml, go.sdk.xml, KeyPromoterXStatistic.xml, Class.java, jdk.table.xml, laf.xml, editor.codeinsight.xml, toStringTemplates.xml, keymap.xml, editor.xml, markdown_navigator.shared.xml, debugger.xml, Mac OS X copy.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,8 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+/**
+ * Created by pfliu on ${YEAR}/${MONTH}/${DAY}.
+ */
+public class ${NAME} {
+}
