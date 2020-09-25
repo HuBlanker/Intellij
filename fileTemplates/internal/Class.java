8867a23d2896283362809029289ca44f5e065be5
liupanfeng <liupanfeng@kanzhun.com>
2020-09-25 12:13:46 +0800

    IU-2018.3.5 <pfliu@huyanshi.local Create baseRefactoring.xml, vim_settings.xml, ide.general.xml, project.default.xml, equalsHashCodeTemplates.xml, lombok-plugin.xml, other.xml, Bash Script.sh, Default.xml, file.template.settings.xml, find.xml, ui.lnf.xml, filetypes.xml, projectView.xml, Python Script.py, AndroidComments.xml, plugin_ui.xml, colors.scheme.xml, KeyPromoterXSettings.xml, path.macros.xml, databaseDrivers.xml, templates.xml, GrepConsole.xml, huyanshi.xml, go.sdk.xml, scala.xml, KeyPromoterXStatistic.xml, Class.java, jdk.table.xml, laf.xml, editor.codeinsight.xml, toStringTemplates.xml, keymap.xml, editor.xml, markdown_navigator.shared.xml, debugger.xml, Mac OS X copy.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,11 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+/**
+ * @author pfliu
+ * @date   ${YEAR}/${MONTH}/${DAY}.
+ * @brief  ${BRIEF}
+ */
+@Slf4j
+public class ${NAME} {
+}
